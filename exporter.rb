bcc15618047c7f95ba1f33e9c2aaab3022322dff
Jimmy Ngu <jimmynguyc@gmail.com>
2025-07-22 18:24:38 +0800

    update

--- exporter.rb
+++ exporter.rb
@@ -1,8 +1,11 @@
-require 'selenium-webdriver'
-require 'nokogiri'
-require 'io/console'
-require 'yaml'
-require 'byebug'
+require "selenium-webdriver"
+require "nokogiri"
+require "io/console"
+require "yaml"
+require "byebug"
+require "dotenv"
+
+Dotenv.load
 
 def finishLoading(browser, wait)
   begin
@@ -17,7 +20,7 @@ def finishLoading(browser, wait)
 end
 
 def sanitize_td(text)
-  text.split('  ').map(&:strip).reject(&:empty?).join(' ')
+  text.split("  ").map(&:strip).reject(&:empty?).join(" ")
 end
 
 def retryable_click(element)
@@ -31,9 +34,9 @@ def parse_table(browser, wait, days, type, first = true, transactions = [], page
     puts "Looking for #{days} days of transactions ..."
     finishLoading(browser, wait)
     wait.until do
-      browser.find_element(:id, 'daysType').displayed?
+      browser.find_element(:id, "daysType").displayed?
     end
-    retryable_click(browser.find_element(:id, 'daysType'))
+    retryable_click(browser.find_element(:id, "daysType"))
     wait.until do
       browser.find_element(:xpath, "//span[text()='Last #{days} days']").displayed?
     end
@@ -42,13 +45,13 @@ def parse_table(browser, wait, days, type, first = true, transactions = [], page
 
   finishLoading(browser, wait)
   wait.until do
-    browser.find_element(:xpath, '//table').displayed?
+    browser.find_element(:xpath, "//table").displayed?
   end
   puts "Parsing page #{page} ..."
-  table_html = browser.find_element(:xpath, '//table').attribute('innerHTML')
+  table_html = browser.find_element(:xpath, "//table").attribute("innerHTML")
   html_doc = Nokogiri::HTML(table_html)
-  html_doc.search('tr').each do |tr|
-    tds = tr.search('td')
+  html_doc.search("tr").each do |tr|
+    tds = tr.search("td")
     next unless tds.length > 0
 
     sign = /negativeAmount/.match(tds[3].inner_html) ? -1 : 1
@@ -57,7 +60,7 @@ def parse_table(browser, wait, days, type, first = true, transactions = [], page
     transactions << {
       date: date.to_s,
       memo: sanitize_td(tds[1].text),
-      amount: sign * sanitize_td(tds[3].text).gsub(/RM|,/, '').to_f
+      amount: sign * sanitize_td(tds[3].text).gsub(/RM|,/, "").to_f
     }
 
     next_button = begin
@@ -91,9 +94,9 @@ def export_to_file(browser, wait, acc_name, days, type)
   finishLoading(browser, wait)
   retryable_click(browser.find_element(:xpath, "//span[text()='#{acc_name}']"))
   transactions = parse_table(browser, wait, days, type).sort_by { |t| t[:date] }
-  puts 'Done parsing tables ...'
+  puts "Done parsing tables ..."
 
-  f = File.open("exports/#{acc_name}.qif", 'w')
+  f = File.open("exports/#{acc_name}.qif", "w")
   transactions.each do |t|
     trx = <<~TRX
       !Type:Bank
@@ -111,61 +114,62 @@ end
 
 # ===========
 
-printf 'Username: '
-username = ENV['VERBOSE'] ? gets.chomp : STDIN.noecho(&:gets).chomp
+printf "Username: "
+username = ENV["USERNAME"] || (ENV["VERBOSE"] ? gets.chomp : $stdin.noecho(&:gets).chomp)
 printf "\nPassword: "
-password = ENV['VERBOSE'] ? gets.chomp : STDIN.noecho(&:gets).chomp
+password = ENV["PASSWORD"] || (ENV["VERBOSE"] ? gets.chomp : $stdin.noecho(&:gets).chomp)
 
 puts "\nLaunching website ..."
 wait = Selenium::WebDriver::Wait.new(timeout: 60)
 options = Selenium::WebDriver::Chrome::Options.new
-options.headless! unless ENV['HEAD'] # remove to debug
+options.headless! unless ENV["HEAD"] # remove to debug
 browser = Selenium::WebDriver.for(:chrome, options: options)
 browser.manage.window.resize_to(1024, 768)
-browser.navigate.to 'https://www.maybank2u.com.my/home/m2u/common/login.do'
+browser.navigate.to "https://www.maybank2u.com.my/home/m2u/common/login.do"
 
 wait.until do
-  browser.find_element(:id, 'username').displayed?
+  browser.find_element(:id, "username").displayed?
 end
 
-puts 'Entering username ...'
-browser.find_element(:id, 'username').send_keys username
-retryable_click(browser.find_element(:name, 'button'))
+puts "Entering username ..."
+browser.find_element(:id, "username").send_keys username
+retryable_click(browser.find_element(:name, "button"))
+binding.irb
 
-puts 'Verification image loaded ...' if wait.until do
-  browser.find_element(:class, 'btn-success').displayed?
+puts "Verification image loaded ..." if wait.until do
+  browser.find_element(:class, "btn-success").displayed?
 end
 
 # puts "Is the verification image correct? [Y/n]: "
 # answer = gets.chomp
 # raise "Invalid verification image!" if answer != "Y"
 
-retryable_click(browser.find_element(:class, 'btn-success'))
+retryable_click(browser.find_element(:class, "btn-success"))
 
 wait.until do
-  browser.find_element(:id, 'my-password-input').displayed?
+  browser.find_element(:id, "my-password-input").displayed?
 end
 
-puts 'Entering password ...'
-browser.find_element(:id, 'my-password-input').send_keys password
-retryable_click(browser.find_element(:class, 'btn-success'))
+puts "Entering password ..."
+browser.find_element(:id, "my-password-input").send_keys password
+retryable_click(browser.find_element(:class, "btn-success"))
 
-puts 'Successful login' if wait.until do
+puts "Successful login" if wait.until do
   /Your last login was on/.match(browser.page_source)
 end
 
-to_import = YAML.load_file('accounts.yml')
+to_import = YAML.load_file("accounts.yml")
 
-accounts = to_import['accounts']
+accounts = to_import["accounts"]
 accounts.each do |acc|
-  export_to_file(browser, wait, acc['name'], acc['days'], :accounts)
+  export_to_file(browser, wait, acc["name"], acc["days"], :accounts)
 end
 
-cards = to_import['cards']
+cards = to_import["cards"]
 cards.each do |acc|
-  export_to_file(browser, wait, acc['name'], 0, :cards)
+  export_to_file(browser, wait, acc["name"], 0, :cards)
 end
 
-puts 'Done!!!'
+puts "Done!!!"
 
 browser.quit
